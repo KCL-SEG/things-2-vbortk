2eb4576b527dd7cd0836920b6aa0678ae76f2b63
Vytas <vboloki@gmail.com>
2023-11-24 13:38:52 +0000

    Implement ThingsForm with functionality

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms, Textarea, NumberInput
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity'] #excluding created_at for now
+    
--- things/views.py
+++ things/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method=='POST':
+        form = ThingForm(request.POST)
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
