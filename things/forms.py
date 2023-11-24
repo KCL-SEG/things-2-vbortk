9bc091255a0135c566e2455f0590d1abe9d779a1
Vytas <vboloki@gmail.com>
2023-11-24 13:45:41 +0000

    Fix test errors

--- things/forms.py
+++ things/forms.py
@@ -9,4 +9,8 @@ class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity'] #excluding created_at for now
+        widgets = {
+            'description': forms.Textarea(attrs={'maxlength': 120}),
+            'quantity': forms.NumberInput(attrs={'type': 'number'}),
+        }
     
